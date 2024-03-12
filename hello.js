95b8966dcd31658cab46cc020cebe67201cd5ca7
Phearathsaly-hue <159137789+Phearathsaly-hue@users.noreply.github.com>
2024-03-12 18:15:37 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,25 @@
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
-let number = Math.ceil(Math.random() * 10000);
 
-btn.onclick = function(){
-    container.style.transform = "rotate("+ number + "deg)"
-    number += Math.ceil(Math.random() * 10000);
-}
+btn.onclick = function () {
+  let number = Math.ceil(Math.random() * 10000);
+  container.style.transform = "rotate(" + number + "deg)";
+
+  // Wait for the rotation animation to complete
+  setTimeout(() => {
+    Swal.fire({
+      title: "Congratulations",
+      html: "You won ",
+      width: 600,
+      padding: "3em",
+      color: "#716add",
+      background: "#fff ",
+      backdrop: `
+              rgba(0,0,123,0.4)
+              url("/images/nyan-cat.gif")
+              left top
+              no-repeat
+                  `,
+    });
+  }, 3000); // Adjust the timeout according to your animation duration
+};
